acbc81997ca65df00206cd11a03754e3c89eb0bf
Sad Drama <justmysk@inbox.lv>
2026-03-04 15:05:22 +0200

    speciali sabojaju testu prieks CI

--- test_kalkulators.py
+++ test_kalkulators.py
@@ -5,4 +5,4 @@ from kalkulators import saskaitit
 class TestKalkulators(unittest.TestCase):
     def test_saskaitit(self):
         rezultats = saskaitit(3, 3)
-        self.assertEqual(rezultats, 6)
+        self.assertEqual(rezultats, 5)
